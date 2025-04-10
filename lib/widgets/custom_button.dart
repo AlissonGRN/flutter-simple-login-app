cfb9055cb87533c104378078b54582c4c0f35516
AlissonGRN <alissonnascimento.dev@gmail.com>
2025-04-10 00:58:44 -0300

    feat: add CustomButton component

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.backgroundColor = Colors.blue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          padding: const EdgeInsets.symmetric(vertical: 16),
+        ),
+        onPressed: onPressed,
+        //TO-DO create AppTheme class
+        child: Text(text),
+      ),
+    );
+  }
+}
